b8a1fd2c5eede5a81bd6f0407d1ad2dc4afb3d80
aryo <aryo.r.h@gmail.com>
2021-07-21 08:51:04 +1200

    Improve regex

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-var sourceMap = require('source-map');
-var fs = require('fs');
+const sourceMap = require("source-map");
+const fs = require("fs");
 
-fs.readFile('./abc.js', 'utf8', async function (err, data) {
+const stacktrace = `
+Unhandled JS Exception: TypeError: null is not an object (evaluating 'u[f]'), stack:
+K@3641:14811
+B@3641:14040
+`;
+const filePath = "./smap.map.js";
+
+fs.readFile(filePath, "utf8", async function (err, data) {
     var smc = await new sourceMap.SourceMapConsumer(data);
-    const a = [ "1:332811", "1:170490","1:1134227"];
-    a.forEach(lineCol => {
-        const [line, column] = lineCol.split(":");
-        console.log(smc.originalPositionFor({
-            line: parseInt(line),
-            column: parseInt(column)
-        }));
-    })
-    console.log(smc.originalPositionFor({
-        line: parseInt(2),
-        column: parseInt(406877)
-    }));
+    const stack = stacktraceParser.parse(stacktrace);
+
+    const header = stacktrace.split("\n").find(line => line.trim().length > 0);
+    header && console.log(header);
+
+    const lineCols = stacktrace.match(/[^.*\:\@](\d+:\d+)[^\)\n]/g);
+    lineCols.forEach(lineCol => {
+        const [mappedLine, mappedCol] = lineCol.split(":");
+        const { source, line, column, name } = smc.originalPositionFor({
+            line: parseInt(mappedLine),
+            column: parseInt(mappedCol),
+        })
+        console.log(`  at ${name} (${source}:${line}:${column})`);
+    });
 });
