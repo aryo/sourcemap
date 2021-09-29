93fdd02cf02628531e69a72b8809ed1cd7cf9c9a
aryo <aryo.r.h@gmail.com>
2021-09-30 10:20:24 +1300

    Fix regex

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ const sourceMap = require("source-map");
 const fs = require("fs");
 
 const stacktrace = `
-Unhandled JS Exception: TypeError: null is not an object (evaluating 'u[f]'), stack:
-K@3641:14811
-B@3641:14040
+Error: Unhandled error.
+  at n.s.emit(app:///build/static/js/2.2d907962.chunk.js:2:143638)
+  at n.<anonymous>(app:///build/static/js/2.2d907962.chunk.js:2:3039390)
+  at l(app:///build/static/js/2.2d907962.chunk.js:2:2583933)
+  at Generator._invoke(app:///build/static/js/2.2d907962.chunk.js:2:2583721)
+  at Generator.throw(app:///build/static/js/2.2d907962.chunk.js:2:2584358)
+  at l(app:///build/static/js/2.2d907962.chunk.js:2:2583933)
+  at n(app:///build/static/js/2.2d907962.chunk.js:2:2584419)
+  at ? (app:///build/static/js/2.2d907962.chunk.js:2:2584595)
 `;
-const filePath = "./smap.map.js";
+const filePath = "./6.45.0.js";
 
 fs.readFile(filePath, "utf8", async function (err, data) {
     var smc = await new sourceMap.SourceMapConsumer(data);
-    const stack = stacktraceParser.parse(stacktrace);
 
     const header = stacktrace.split("\n").find(line => line.trim().length > 0);
     header && console.log(header);
 
-    const lineCols = stacktrace.match(/[^.*\:\@](\d+:\d+)[^\)\n]/g);
+    const lineCols = stacktrace.match(/[^.*](\d+:\d+)[^\)]/g);
     lineCols.forEach(lineCol => {
-        const [mappedLine, mappedCol] = lineCol.split(":");
+        const [_, mappedLine, mappedCol] = lineCol.split(":");
         const { source, line, column, name } = smc.originalPositionFor({
             line: parseInt(mappedLine),
             column: parseInt(mappedCol),
